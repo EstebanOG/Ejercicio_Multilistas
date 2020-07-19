0eae1cd13c98f6321febcae8beb99e9f0586fa88
Esteban <juanesteban199911@gmail.com>
2020-07-19 13:28:11 -0500

    Cambios

--- Postres/main.cpp
+++ Postres/main.cpp
@@ -15,7 +15,6 @@ int main(int argc, char** argv) {
 	int i,j,pos,num_ingre,op;
 	bool seguir = true;
 	ingrediente auxI;
-	postre P;
 	lista<postre> listP;
 	while(seguir){
 		cout<<"Menu: "<<endl;
@@ -29,13 +28,14 @@ int main(int argc, char** argv) {
 		cout<<"8. Mostrar lista de postres."<<endl;
 		cout<<"9. Salir."<<endl;
 		cin>>op;
+		postre P;
 		switch(op){
 			case 1:
 				cout<<"Nombre del postre: ";cin>>P.nombre;
 				cout<<"Numero de minutos para su preparacion: ";cin>>P.min_preparacion;
 				cout<<"Cómo se prepara?: ";cin>>P.pasos;
 				cout<<"Cuántos ingredientes tiene?:";cin>>num_ingre;
-				for(j=0;j<num_ingre;j++){
+				for(i=0;i<num_ingre;i++){
 					cout<<"Nombre del ingrediente: ";cin>>auxI.nombre;
 					cout<<"Unidad de medida: ";cin>>auxI.medida;
 					cout<<"Cantidad: ";cin>>auxI.cantidad;
@@ -50,10 +50,32 @@ int main(int argc, char** argv) {
 				listP.borrar(pos);
 				break;
 			case 4:
+				cout<<"Ingrese la posición del postre: ";cin>>pos;
+				listP.buscar(pos,&P);
+				cout<<"Cuántos ingredientes desea agregar?:";cin>>num_ingre;
+				for(i=0;i<num_ingre;i++){
+					cout<<"Nombre del ingrediente: ";cin>>auxI.nombre;
+					cout<<"Unidad de medida: ";cin>>auxI.medida;
+					cout<<"Cantidad: ";cin>>auxI.cantidad;
+					P.i.insertar_final(auxI);
+				}
+				listP.cambiar(pos,P);
 				break;
 			case 5:
 				break;
 			case 6:
+				cout<<"Ingrese la posición del postre: ";cin>>pos;
+				listP.buscar(pos,&P);
+				cout<<"Los ingredientes del postre son:"<<endl;
+				pos = 1;
+				while(pos<=P.i.get_tam())
+				{
+					P.i.buscar(pos, &auxI);
+					cout<<pos<<"."<<"Nombre: "<<auxI.nombre<<endl;
+					pos++;
+				}
+				cout<<"Cuál ingrediente desea eliminar?: ";cin>>pos;
+				P.i.borrar(pos);
 				break;
 			case 7:
 				cout<<"Ingrese la posición del postre a observar: ";cin>>pos;
