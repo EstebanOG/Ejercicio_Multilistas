96bdb28eaa9f50a30b2a881d619896fe039b8ee0
Esteban <juanesteban199911@gmail.com>
2020-07-19 00:01:02 -0500

    Se agrega buscar por posición

--- Postres/main.cpp
+++ Postres/main.cpp
@@ -12,7 +12,7 @@ struct postre{string nombre, pasos;
 			  lista<ingrediente> i;
 };
 int main(int argc, char** argv) {
-	int num_postre,i,pos,num_ingre;
+	int num_postre,i,j,pos,num_ingre;
 	ingrediente auxI;
 	postre P;
 	lista<postre> listP;
@@ -23,7 +23,7 @@ int main(int argc, char** argv) {
 		cout<<"Numero de minutos para su preparacion: ";cin>>P.min_preparacion;
 		cout<<"Cómo se prepara?: ";cin>>P.pasos;
 		cout<<"Cuántos ingredientes tiene?:";cin>>num_ingre;
-		for(i=0;i<num_ingre;i++){
+		for(j=0;j<num_ingre;j++){
 			cout<<"Nombre del ingrediente: ";cin>>auxI.nombre;
 			cout<<"Unidad de medida: ";cin>>auxI.medida;
 			cout<<"Cantidad: ";cin>>auxI.cantidad;
@@ -32,10 +32,23 @@ int main(int argc, char** argv) {
 		listP.insertar_final(P);
 	}
 	pos=1;
+	cout<<"Lista de postres:"<<endl;
 	while(pos<=listP.get_tam())
 	{
 		listP.buscar(pos, &P);
-		cout<<P.nombre<<endl<<P.min_preparacion<<endl<<P.pasos<<endl;
+		cout<<pos<<". "<<P.nombre<<endl;
+		pos++;
+	}
+	
+	cout<<"Ingrese la posición del postre a observar: ";cin>>pos;
+	listP.buscar(pos,&P);
+	cout<<"Nombre: "<<P.nombre<<endl<<"Minutos de preparacion: "<<P.min_preparacion<<endl<<"Preparación: "<<P.pasos<<endl;
+	cout<<"Ingredientes:"<<endl;
+	pos=1;
+	while(pos<=P.i.get_tam())
+	{
+		P.i.buscar(pos, &auxI);
+		cout<<"Nombre: "<<auxI.nombre<<endl<<"Medida: "<<auxI.medida<<endl<<"Cantidad: "<<auxI.cantidad<<endl;
 		pos++;
 	}
 	return 0;
